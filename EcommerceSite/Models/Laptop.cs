7b93cfacf02809fbd4ac1a866e85863714a7524a
manjusomu <manjusomu2001@gmail.com>
2024-02-11 19:16:29 +0530

    Ecommerce

---
+++ EcommerceSite/Models/Laptop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceSite.Models
+{
+    public class Laptop
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ EcommerceSite/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceSite.Models
+{
+    public class Product
+    {
+        public Laptop LapDetails(int id)
+        {
+            Laptop lap = new Laptop();
+            switch(id)
+            {
+                case 1:
+                    lap = new Laptop()
+                    {
+                        ProductId = 101,
+                        Brand = "lenovo",
+                        Model = "i3",
+                        Price = 27000
+                    };
+                    break;
+                case 2:
+                    lap = new Laptop()
+                    {
+                        ProductId = 105,
+                        Brand = "samsung",
+                        Model = " corei7",
+                        Price = 82000
+                    };
+                    break;
+                case 3:
+                    lap = new Laptop()
+                    {
+                        ProductId = 102,
+                        Brand = "Hp",
+                        Model = "i7",
+                        Price = 45000
+                    };
+                    break;
+                case 4:
+                    lap = new Laptop()
+                    {
+                        ProductId = 103,
+                        Brand = "Dell",
+                        Model = "core i5",
+                        Price = 53300
+                    };
+                    break;
+            }
+            return lap;
+        }
+    }
+}
